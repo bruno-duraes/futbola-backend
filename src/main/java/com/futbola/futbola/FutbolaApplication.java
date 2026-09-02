af4e304c93830430d480e4b06f3ce5209bd0e2e3
Bruno Durães <brunodur4es@gmail.com>
2026-09-01 21:24:43 -0300

    Estrutura inicial

---
+++ src/main/java/com/futbola/futbola/FutbolaApplication.java
@@ -0,0 +1,13 @@
+package com.futbola.futbola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutbolaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutbolaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/futbola/futbola/FutbolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.futbola.futbola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FutbolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
